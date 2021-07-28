e850869046a0749a286f02297f3cecc6ed6097f6
lonnalu <lonnalu@gmail.com>
2021-07-28 14:32:34 -0700

    Connected to express and created test get request

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 const express = require('express');
+const app = express();
+const PORT = 4000;
+
+app.get('/', (req, res) => res.send('Test page'));
+
+
+app.listen(PORT, function() {
+    console.log(`Connected to Port ${PORT} Yay`);
+});
